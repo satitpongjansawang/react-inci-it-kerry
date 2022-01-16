454690a453a0f3a17cf2376653c621e4a57bba51
satitpongjansawang <satitpongjansawang@gmail.com>
2022-01-16 20:01:27 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {//LINE_UserID	pictureUrl	displayname	os	language	email	phone
   const [whocall , setWhoCall] = useState('');
   const [sel3, setSel3] = useState('');//plant or warehouse location
 
-  const [events , setEvents] = useState([{"type": "postIncident"}]);
+  const [events , setEvents] = useState('');
 
   const initLine = () => {
     liff.init({ liffId : "1656730001-Gv2VMYDo"}, () => {
@@ -39,6 +39,7 @@ function App() {//LINE_UserID	pictureUrl	displayname	os	language	email	phone
       setEmail(liff.getDecodedIDToken().email);
       setOS(liff.getOS());
       setLang(liff.getLanguage());
+      setEvents({"type":"postIncident"});
     }).catch(err => console.error(err));
   }
 
